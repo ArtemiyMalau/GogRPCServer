c913d6406de63170d469dc1e549982a9e96bd3ac
ArtemiyMalau <ya.fantic2012@yandex.ru>
2022-10-09 01:08:54 +0300

    Replace proto files

--- src/server/proto/productsexplorer.pb.go
+++ src/server/proto/productsexplorer.pb.go
@@ -4,7 +4,7 @@
 // 	protoc        v3.21.6
 // source: products_explorer/productsexplorer/productsexplorer.proto
 
-package productsexplorer
+package pb
 
 import (
 	protoreflect "google.golang.org/protobuf/reflect/protoreflect"
