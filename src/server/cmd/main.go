3627d0c1b4ff1b4bcb923fd0fd9f00b3c457b793
ArtemiyMalau <ya.fantic2012@yandex.ru>
2022-10-09 11:05:17 +0300

    Implement server's entry-point method

--- src/server/cmd/main.go
+++ src/server/cmd/main.go
@@ -1,5 +1,26 @@
 package main
 
-func main() {
+import (
+	"context"
+	"grpc_server/config"
+	"grpc_server/internal/adapters/db/mongodb"
+	"grpc_server/internal/adapters/grpc"
+	"grpc_server/internal/application/api"
+	"log"
+)
 
+func main() {
+	cfg := config.GetConfig()
+	dbAdapter, err := db.NewAdapter(context.Background(), cfg.MongoDB.Host, cfg.MongoDB.Port,
+		cfg.MongoDB.Username, cfg.MongoDB.Password, cfg.MongoDB.Database, cfg.MongoDB.AuthDB,
+		cfg.MongoDB.Collections.Product, cfg.MongoDB.Collections.Document,
+	)
+	if err != nil {
+		log.Fatalf("Failed to connect to database. Error: %v", err)
+		return
+	}
+
+	application := api.NewApplication(dbAdapter)
+	grpcAdapter := grpc.NewAdapter(application)
+	grpcAdapter.Run()
 }
